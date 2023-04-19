539a962ffedaf75837e3274bc51fafd9799556c7
corbanvilla <corbanvilla@gmail.com>
2023-04-20 01:32:38 +0400

    move files

--- app/(latestIssueNavbar)/(landingpage)/page.tsx
+++ app/(latestIssueNavbar)/(landingpage)/page.tsx
@@ -3,17 +3,20 @@ export const config = {
     regions :  [ 'fra1' ] ,   // only execute this function on iad1
 };
 
+import { cache } from 'react';
 import { Metadata } from 'next';
-import { getLatestPublishedIssue, getIssueArticles } from '../../db';
-import Issue from '../../components/Issue';
+import { getLatestPublishedIssue, getIssueArticles } from '../../../db';
+import Issue from '../../../components/Issue';
 
 export const metadata: Metadata = {
   description: 'The latest issue from The Gazelle team at NYU Abu Dhabi',
 };
 
+const latestIssues = cache(getLatestPublishedIssue);
+
 export default async function Page() {
   // Fetch latest article data
-  const issue = await getLatestPublishedIssue();
+  const issue = await latestIssues();
   const issueArticles = await getIssueArticles(issue);
 
   // Render the page
