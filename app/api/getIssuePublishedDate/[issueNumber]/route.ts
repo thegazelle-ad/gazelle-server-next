7c51f07a30dc75642b2caddcf227a3b155761957
corbanvilla <corbanvilla@gmail.com>
2023-04-20 14:51:32 +0400

    fix route

--- app/api/getIssuePublishedDate/[issueNumber]/route.ts
+++ app/api/getIssuePublishedDate/[issueNumber]/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 import { getIssue } from '../../../../db'
 
-export async function GET({ params }: { params: { issueNumber: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { issueNumber: string } }) {
     // convert to number if possible, else error
     const issueNumber = Number(params.issueNumber);
     if (isNaN(issueNumber)) {
