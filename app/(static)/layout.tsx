c00a870fa41e12aaf2e24dd3ece1ded19afeec37
corbanvilla <corbanvilla@gmail.com>
2023-04-30 17:39:22 +0400

    add static pages

--- app/(static)/layout.tsx
+++ app/(static)/layout.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-
-export default function ArticleLayout ({ children }: { children: React.ReactNode }) {
-  return (
-    <div className='flex flex-col items-start justify-center min-h-screen max-w-[1000px] mx-auto'>
-      <div className='flex flex-col items-start justify-center max-w-[529px] mx-auto py-2 md:py-8'>
-        {/* Page */}
-        {children}
-      </div>
-    </div>
-  );
-};
+export default function Layout({ children }: { children: React.ReactNode}) {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="flex flex-col items-center justify-center max-w-[700px] w-4/5 leading-relaxed">
+                {children}
+            </div>
+        </div>
+    )
+}
