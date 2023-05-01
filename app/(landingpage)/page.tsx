b752712727597ffd1bdaa7c52f88ffe5fbfe3492
corbanvilla <corbanvilla@gmail.com>
2023-05-01 18:46:59 +0400

    fix config

--- app/(landingpage)/page.tsx
+++ app/(landingpage)/page.tsx
@@ -1,5 +1,5 @@
 export const config = {
-    runtime: 'experimental-edge',   // this is a pre-requisite   
+    runtime: 'edge',   // this is a pre-requisite   
     regions :  [ 'fra1' ] ,   // only execute this function on iad1
 };
 export const revalidate = 300;
