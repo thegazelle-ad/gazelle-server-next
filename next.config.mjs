0496869a4a360ef454b8283619bd9db0030cb4a0
corbanvilla <corbanvilla@gmail.com>
2023-05-18 14:41:37 +0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -53,7 +53,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, s-maxage=300, stale-while-revalidate=3600',
+            value: 'public, s-maxage=600, stale-while-revalidate=3600',
           }
         ]
       }
